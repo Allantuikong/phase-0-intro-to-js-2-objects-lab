004947c749f245dfec9741048509ec1a6f5f6416
Allan Kipkorir <allantuix@gmail.com>
2024-04-05 11:51:04 +0300

    Practiced working with objects

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution in this file!
+const employee = {
+    name: " James Bond",
+    streetAddress: "Mombasa",
+};
+
+//  updateEmployeeWithKeyAndValue(employee, key, value) - Non Destructively
+function updateEmployeeWithKeyAndValue(obj,key,value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "Jackie Chan",
+);
+
+// console.log(newEmployee);
+
+// destructivelyUpdateEmployeeWithKeyAndValue 
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value) {
+        obj[key] = value;
+        return obj;
+   
+}
+const modifiedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Batman");
+console.log(modifiedEmployee);
+console.log(employee)
+
+// deleteFromEmployeeByKey(employee, key) - Non Destructively
+
+function deleteFromEmployeeByKey(obj, key) {
+   const newObj = {...obj}
+   delete newObj[key];
+   return newObj;
+};
+
+const employee2 = deleteFromEmployeeByKey(employee,"name");
+console.log(employee2);
+console.log(employee);
+
+// destructivelyDeleteFromEmployeeByKey
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj
+}
+
+const modifyEmp = destructivelyDeleteFromEmployeeByKey(employee, "name");
+console.log(modifyEmp);
+console.log(employee);
